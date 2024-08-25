92fa512dfcbc1f23ebbbb1e3ef9786de7e91f45e
shreydd <shreyasramesh08@gmail.com>
2024-08-25 16:46:08 +0530

    feat: working slider for popup

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,11 +1,25 @@
 import { LucidePause, LucidePlay } from 'lucide-react'
 import toreUp from '../../public/images/tore-up-ixd.png'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import clsx from 'clsx'
+import { Slider } from './ui/slider'
 
 export function Popup() {
 
   const [isExpanded, setIsExpanded] = useState(false)
+
+  const [value, setValue] = useState(0);
+  const max = 350; // Adjust as needed
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isExpanded) {
+        setValue((prevValue) => (prevValue >= max ? 0 : prevValue + 1));
+      }
+    }, 100); // Change the interval as needed (1000ms = 1 second)
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [isExpanded]);
 
   const handleClick = () => {
     setIsExpanded(!isExpanded)
@@ -15,6 +29,16 @@ export function Popup() {
     <section className="section-card">
       <div className={clsx("border border-gray-200 rounded-2xl group hover:shadow-2xl shadow-lg p-4 flex flex-wrap gap-4 w-full md:max-w-[400px] transition-all duration-300", isExpanded && 'flex-col')}>
         <img src={toreUp} alt='tore up cover image' className={clsx("rounded-lg transition-all duration-300", isExpanded ? 'w-full h-[368px]' : 'size-20')} />
+        {
+          isExpanded &&
+          <Slider
+            value={[value]}
+            max={max}
+            step={1}
+            className='transition-all duration-300'
+            onValueChange={setValue}
+          />
+        }
         <div className="flex flex-1 justify-between items-center">
           <div className="">
             <h3 className='text-lg font-semibold'>Tore Up</h3>
